6f89498be85d83f060c632989d5e933d46bb1d39
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-24 12:09:55 -0500

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -2,10 +2,12 @@
 
 public class Calculator{
     public int Max(int first, int second){
-        return 0;
+        int max = Math.Max(first, second);
+        return max;
     }
 
     public double Round(double value){
+        double round = Math.Round(Value);
         return 0.00;
     }
 }
